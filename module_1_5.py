70e7755e9da05ff6f2b225d01845b6a3e4e3d5c8
Valeriy-Perminov <perminov.ahura999@gmail.com>
2024-09-07 19:57:11 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,19 @@
+#2
+immutable_var = 1, 2.0, 3, 'a', 'b'
+print(type(immutable_var))
+print(immutable_var)
+
+#3
+# TypeError: 'tuple' object does not support item assignment
+# Главное свойство кортежа - это невозможность изменить содержимое кортежа после его создания
+# после его создания
+
+#4
+mutable_list = 'a', 'b', 1, 2.0, 3
+print(type(str(mutable_list)))
+print(mutable_list)
+mutable_list = (['a','b'], 1, 2, 3)
+print(mutable_list)
+mutable_list = (['a','b'], 1, 2, 3)
+mutable_list [0][0] = 4
+print(mutable_list)
